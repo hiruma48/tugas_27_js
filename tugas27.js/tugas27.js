a68beb59a30962b91be39c7ce185a241df4794e0
tri ariyanto <triyanto129@gmail.com>
2023-05-25 06:16:31 +0700

    tugas 27 js
    mengumpulkan tugas

---
+++ tugas27.js/tugas27.js
@@ -0,0 +1,11 @@
+function panggilstring(){
+    var a = ["no","rumah saya",30,"A"];
+    var b = ["Jalan pangeran Tirtayasa No", 70];
+    
+    hasil1=String(a.join(" "));
+    console.log(hasil1);
+    hasil2=String(b.join(" "));
+    console.log(hasil2);
+    
+}
+panggilstring()
